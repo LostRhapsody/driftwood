3d2dc55d6e9188028796066f60942180e7c8cd28
Evan Robertson <evan.robertson77@gmail.com>
2024-12-22 17:35:01 -0500

    feat: added selected site context to settings and posts, removed placeholder

--- driftwood/app/posts/page.tsx
+++ driftwood/app/posts/page.tsx
@@ -1,23 +1,13 @@
 "use client";
-import { ChevronRight } from "lucide-react";
-import { cn } from "@/lib/utils";
-import AnimatedGradientText from "@/components/ui/animated-gradient-text";
+import {useSelectedSite} from "@/contexts/SelectedSiteContext";
 
 export default function home() {
-    return (
+  const { selectedSite } = useSelectedSite();
+  return (
     <div className="items-center justify-center">
-      SiteName's Posts
-      <AnimatedGradientText>
-        🎉 <hr className="w-px h-4 mx-2 bg-gray-300 shrink-0" />{" "}
-        <span
-          className={cn(
-            "inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent",
-          )}
-        >
-          This is the Posts page
-        </span>
-        <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
-      </AnimatedGradientText>
+      {selectedSite.name} - Posts
+      <br />
+      TODO: Add posts here
     </div>
   );
 }
